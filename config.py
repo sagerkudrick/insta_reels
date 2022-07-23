8d2a328bed8e46f54d963af5ce36f969d901fe12
Sager Kudrick <sagerkudrick@hotmail.com>
2022-07-23 13:51:57 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# replace with your username and password
+# file location is your the reel you want to upload, \'s need to be doubled (c:\desktop -> c:\\desktop)
+#post contents is what your post will say. use \n to add new lines, use #hashtag as normal 
+
+username = 'username'
+password = 'password'
+filelocation = 'C:\\Users\\me\\example\\file_to_upload.mp4' #C:\Users\me\Documents should be changed to C:\\Users\\me\\Documents 
+postcontents = "post contents \n \n #hashtag_example \n #hashtag_example \n #hashtag_example \n #hashtag_example"
